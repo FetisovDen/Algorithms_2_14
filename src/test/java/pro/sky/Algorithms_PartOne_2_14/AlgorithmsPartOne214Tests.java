ec0b752c53478619b34ce0321b0168c658b5f26f
555 <fetisovden@yandex.ru>
2022-10-17 15:37:57 +0300

    Alg_part1_2_14

--- src/test/java/pro/sky/Algorithms_PartOne_2_14/AlgorithmsPartOne214Tests.java
+++ src/test/java/pro/sky/Algorithms_PartOne_2_14/AlgorithmsPartOne214Tests.java
@@ -1,10 +1,10 @@
-package pro.sky.EmployeeWithList;
+package pro.sky.Algorithms_PartOne_2_14;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmployeeWithListApplicationTests {
+class AlgorithmsPartOne214Tests {
 
 	@Test
 	void contextLoads() {
